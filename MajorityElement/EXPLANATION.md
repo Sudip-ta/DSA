f8e99b9bdc922d9fb0a75fa1585a5afe1449a36f
Sudipta <sudiptabhatta34@gmail.com>
2026-06-28 19:24:57 +0530

    my solution

--- MajorityElement/EXPLANATION.md
+++ MajorityElement/EXPLANATION.md
@@ -1,3 +1,10 @@
 # Brute force approach  
+
+* Iterate through the array to select each element one by one.
+
+* For each selected element, run another loop to count its occurrences in the given array.
+
+* If the occurrence of any element is greater than the floor of (N/2), return that element immediately as the majority element.
+
 # Better approach  
 # Optimal approach  
---
+++ MajorityElement/MajorityElementMyApproach.java
@@ -0,0 +1,16 @@
+package MajorityElement;
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int majorityElement = nums[0];
+        int count = 1;
+        for(int i = 1;i<nums.length;i++){
+            if(count == 0) {
+                majorityElement = nums[i];
+            }
+            if(nums[i] == majorityElement) count ++;
+            else count--;
+        }
+        return majorityElement;
+    }
+}
